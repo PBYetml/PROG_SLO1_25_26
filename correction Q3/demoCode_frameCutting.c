9b2581e85f703334c9157cc9dfc20185ab789eb8
Louis Cardinaux <loucardinau@etmlnet.local>
2024-11-19 12:51:55 +0100

    correction Q6

--- correction Q3/demoCode_frameCutting.c
+++ correction Q3/demoCode_frameCutting.c
@@ -8,8 +8,8 @@
 //
 // Version				: 0.3
 //
-// Description          : source file including the function declaration to the Q6 
-//						  test 
+// Description          : source file including the function declaration to the Q6
+//						  test
 //
 // Remarks				: complet programm
 //----------------------------------------------------------------------------------*/
@@ -22,10 +22,10 @@
 
 /* -----------------------------------------------------------------------------------
 * Function Name                     : ClearBufferKeyBoard
-* Input - Ouput - I/O parameters    : => 
-									  => integer 
+* Input - Ouput - I/O parameters    : =>
+									  => integer
 									  => N/A
-* Description                       : clear the Buffer KeyBoard 
+* Description                       : clear the Buffer KeyBoard
 * Modification Date                 : 08.11.2024
 * Notes                             : https://www.tutorialspoint.com/c_standard_library/c_function_getchar.htm
 ----------------------------------------------------------------------------------- */
@@ -35,7 +35,7 @@ int8_t ClearBufferKeyBoard()
 	char receiveCharcter;
 
 	//-- loop to clear the buffer linked at the keyboard --// 
-	do 
+	do
 	{
 		//-- keyboard buffer reading --//  
 		receiveCharcter = getchar();
@@ -47,15 +47,15 @@ int8_t ClearBufferKeyBoard()
 
 /* -----------------------------------------------------------------------------------
 * Function Name                     : AssembyDatas
-* Input - Ouput - I/O parameters    : => 4x input : 
+* Input - Ouput - I/O parameters    : => 4x input :
 *										voltageData		(Integer - 1bytes)
 *										currentData		(Integer - 1bytes)
 *										powerData		(Integer - 1bytes)
 *										resistorData	(Integer - 1bytes)
-									  => 1x ouput : 
+									  => 1x ouput :
 										frame (Integer - 4bytes)
 									  => N/A
-* Description                       : Q6 solution, assembly of 4 bytes in an only 
+* Description                       : Q6 solution, assembly of 4 bytes in an only
 									  variable
 * Modification Date                 : 12.11.2024
 * Notes                             : there may be two variants for this function
@@ -64,32 +64,32 @@ int8_t ClearBufferKeyBoard()
 int AssembyDatas(int8_t voltageData, int8_t currentData, int8_t powerData, int8_t resistorData)
 {
 	//-- variable declarations --// 
-	int frame, middleFrame; 
+	int frame, middleFrame;
 
 	//-- preparation of frame --// 
 	//-- variant 1) LSB on right 
 	frame = (voltageData << 24) & 0xFF000000;			// unwrapping of 24bits and masking -> MSB part 	
-	middleFrame = (currentData << 16) & 0x00FF0000; 
+	middleFrame = (currentData << 16) & 0x00FF0000;
 	frame = frame | middleFrame;						// update datas with voltage and cuurent   
-	middleFrame = (powerData << 8) & 0x0000FF00; 
+	middleFrame = (powerData << 8) & 0x0000FF00;
 	frame = frame | middleFrame;						// update datas with voltage, current and power 
-	middleFrame = resistorData & 0x000000FF; 
+	middleFrame = resistorData & 0x000000FF;
 	frame = frame | middleFrame;						// update datas with voltage, current, power, resistor 
-	
-	
-	return frame; 
+
+
+	return frame;
 }
 
 
 /* -----------------------------------------------------------------------------------
 * Function Name                     : DatasCutting
-* Input - Ouput - I/O parameters    : => 1x input : 
-										frame (Integer - 4bytes)  
-									  => 0 output :  
+* Input - Ouput - I/O parameters    : => 1x input :
+										frame (Integer - 4bytes)
+									  => 0 output :
 									  => N/A
-* Description                       : Q6 solution, divinding frame into 4 parts 
+* Description                       : Q6 solution, divinding frame into 4 parts
 * Modification Date                 : 14.11.2024
-* Notes                             : there may be two variants for this function 
+* Notes                             : there may be two variants for this function
 *									  only one of which is implemend
 ----------------------------------------------------------------------------------- */
 void DatasCutting(int frame)
